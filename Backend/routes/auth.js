8b80a583e61be09c34c536dafbe1b3557b9c9777
PabloAlarconArel <pablo.alarcon.arel@gmail.com>
2025-08-08 01:11:56 -0400

    Create route auth

---
+++ Backend/routes/auth.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const router = express.Router();
+const{validateSchema}=require("../middlewares/ValidatorMiddlewares")
+const{registerSchema,loginSchema}= require("../schemas/schemaAuth")
+const {login,register,logout,profile} = require ( "../controllers/controller");
+const {authRequired} = require("../middlewares/validateToken");
+
+router.post("/login",validateSchema(loginSchema),login);
+router.post("/register",validateSchema(registerSchema),register);
+router.post("/logout",logout);
+router.post("/profile",authRequired,profile);
+
+module.exports = router;
