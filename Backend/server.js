01b7359f90dbd2b925152443f7152e95691d1817
PabloAlarconArel <pablo.alarcon.arel@gmail.com>
2025-08-08 01:08:42 -0400

    Create: server

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const express = require("express");
+const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const cors= require("cors");
+const cookieParser = require("cookie-parser");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Conectado a MongoDB");
+
+}).catch((err) => {
+    console.error("Error al conectar a MongoDB:", err);
+    process.exit(1); 
+    
+});
+
+app.use("/api",require("./routes/auth"));
+app.use("/api",require("./routes/productRoute"));
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
