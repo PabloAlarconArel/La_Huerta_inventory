11f9a7a536c2ec9625d28dbebdccdbcce6c8a3ba
PabloAlarconArel <pablo.alarcon.arel@gmail.com>
2025-08-13 22:16:17 -0400

    add origin and credentials cors

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,11 @@ const mongoose = require("mongoose");
 const cors= require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
-app.use(cors());
+app.use(cors(
+    {origin:'http://localhost:5173',
+    credentials:true ,
+    }
+));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }))
