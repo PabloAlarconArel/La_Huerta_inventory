5e8323b67b9fbd445de6bdfd67a1156fb92bb683
PabloAlarconArel <pablo.alarcon.arel@gmail.com>
2025-08-15 22:07:38 -0400

    Fix routes product and auth, add nodemon to project

--- Backend/libs/jwt.js
+++ Backend/libs/jwt.js
@@ -4,7 +4,7 @@ const createAccessToken = (payload) => {
     return new Promise((resolve, reject) =>{
         jwt.sign( payload,
             process.env.JWT_SECRET, {
-                expiresIn: '1h',
+                expiresIn: '24h',
                 },
                 (err,token) => {
                 if (err) reject (err);
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require ('express');
 const router = express.Router();
-const{validateSchema}=require("../middlewares/ValidatorMiddlewares")
-const{registerSchema,loginSchema}= require("../schemas/schemaAuth")
-const {login,register,logout,profile} = require ( "../controllers/controller");
-const {authRequired} = require("../middlewares/validateToken");
+const validateSchema = require("../middlewares/ValidatorMiddlewares")
+const{loginSchema,registerSchema}= require("../schemas/schemaAuth")
+const {login,register,logout,profile} = require ( "../controllers/controller")
+const authRequired = require("../middlewares/validateToken")
 
 router.post("/login",validateSchema(loginSchema),login);
 router.post("/register",validateSchema(registerSchema),register);
