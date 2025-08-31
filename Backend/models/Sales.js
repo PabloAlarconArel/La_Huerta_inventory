296e1ec2156134e26eae3e413fc6fa6c28cf2204
PabloAlarconArel <pablo.alarcon.arel@gmail.com>
2025-08-30 23:53:03 -0400

    create: sales model

---
+++ Backend/models/Sales.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const salesSchema = new mongoose.Schema({
+    products:[{
+        products:{type:mongoose.Schema.Types.ObjectId,ref:"Product",required:true},
+        quantity:{type:Number,required:true},
+        price:{type:Number,required:true}
+    }],
+    total:{type:Number,required:true},
+},{
+    timestamps:true
+});
+
+module.exports= mongoose.model("Sale",salesSchema);  
